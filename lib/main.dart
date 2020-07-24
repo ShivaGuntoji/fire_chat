6716a4490ae93678be348889991ffae5ec94e4a3
ShivaGuntoji <shiva.guntoji007@gmail.com>
2020-07-24 11:43:21 +0530

    befor reinstall

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/welcome_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/chat_screen.dart';
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -12,9 +16,17 @@ class FlashChat extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
+          // ignore: deprecated_member_use
           body1: TextStyle(color: Colors.black54),
         ),
       ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id:(context) => WelcomeScreen(),
+        RegistrationScreen.id:(context) => RegistrationScreen(),
+        LoginScreen.id:(context) => LoginScreen(),
+        ChatScreen.id:(context) => ChatScreen(),
+      },
       home: WelcomeScreen(),
     );
   }
